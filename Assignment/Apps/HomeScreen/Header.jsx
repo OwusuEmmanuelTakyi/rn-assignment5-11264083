9dcc8d248271d7d33b1994bd5f4ed5996b93ed2e
“OwusuEmmanuelTakyi” <owusuemmanueltakyi@gmail.com>
2024-06-26 16:00:10 +0000

    created the header part of the homescreen

---
+++ Assignment/Apps/HomeScreen/Header.jsx
@@ -0,0 +1,12 @@
+import { View,Image  } from 'react-native'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <View>
+      <Image source={{}} 
+         className="rouded-full w-10 h-10"
+      />
+    </View>
+  )
+}
--- Assignment/Apps/Navigations/TabNavigation.jsx
+++ Assignment/Apps/Navigations/TabNavigation.jsx
@@ -1,17 +1,27 @@
-import { View, Text } from 'react-native'
+import { Image, Text } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../Screens/HomeScreen';
 import CardScreen from '../Screens/CardScreen';
 import StatScreen from '../Screens/StatScreen';
 import SettingsScreen from '../Screens/SettingsScreen';
+import homeIcon from '../../assets/home.png';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigation() {
   return (
     <Tab.Navigator screenOptions={{headerShown:false}}>
-        <Tab.Screen name='Home' component={HomeScreen} />
+        <Tab.Screen name='Home' component={HomeScreen} 
+           options={{
+             tabBarLabel:({color}) =>(
+                <Text style={{color:color}}>Home</Text>
+             ),
+             tabBarIcon: ({ color, size }) => (
+                <Image source={homeIcon} style={{ width: size, height: size, tintColor: color }} />
+              ),
+           }}
+        />
         <Tab.Screen name='My card' component={CardScreen} />
         <Tab.Screen name='Statistics' component={StatScreen} />
         <Tab.Screen name='Settings' component={SettingsScreen} />
